979bfe2420ed2d3469c7767b06ea2bfc8097fd1e
olilewis1 <olilewis1@hotmail.co.uk>
2023-09-21 21:43:04 +0100

    added fornt end terminal

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from 'react';
+import playerData from './data/player_data_next_three.json';
+import NextThree from './NextThree';
 import './App.css';
 
 function App() {
+  // Define the playerData state
+  const [playerDataState, setPlayerDataState] = useState({ playerData });   
+
+
+  // Define the input state
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('')
+  const inputRef = useRef()
+  const [outPutCheck, setOutputCheck] = useState(0)
+  const outPutArray = ['$ Input Login Email', '$ Input FPL Password', '$ Input FPL ID']
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' 
+    onClick={e=> {   inputRef.current.focus()}}>
+      {/* <NextThree {...playerDataState.playerData}/> */}
+      {/* Input field */}
+      
+   
+
+      {/* Display the current 'input' state */}
+      <div className='terminal'>{output}</div>
+      <div>{outPutArray[outPutCheck]}</div>
+      <input
+      ref={inputRef}
+        type='text'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        onKeyDown={e => { 
+          if (e.key === 'Enter') { 
+            let newOutput = '' 
+            newOutput = output + '\n' + '$ ' + input + '\n'
+            if( input.includes('@') ) { 
+              newOutput += 'Email'
+              const newOutputCheck = outPutCheck + 1 
+              setOutputCheck(newOutputCheck) 
+            } else if (input === 'login') { 
+              newOutput += 'Login received'
+            } 
+            else { 
+              newOutput += 'Login Details Not Found'
+            }
+            setOutput(newOutput)
+            setInput('')
+          } 
+        }}
+      />
     </div>
   );
 }
---
+++ src/NextThree.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const NextThree = (props) => {
+  console.log(props.GK  )
+  function hi(propsToUse) {
+    const mapping_position = ['GK', 'DEF', 'MID', 'FWD'];
+    
+    mapping_position.forEach(position => {
+      console.log(position + ':');
+      propsToUse[position].forEach(player => {
+        console.log(player);
+      });
+    });
+  }
+  
+  // Call the hi function with your props object
+  hi(props);
+  
+  const mapping_position = ['GK', 'DEF', 'MID', 'FWD']
+  return (
+    <div>
+      {mapping_position.map((position) => (
+        <div className={'test'}key={position}>
+          {position + ':'}
+          {props[position].map((player, index) => (
+            <>
+            <div key={index}>{player.name}</div>
+            <div key={index}>{player.price}</div>
+            <div key={index}>{player.club}</div>
+            <div key={index}>{player.points}</div>
+            </>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default NextThree
